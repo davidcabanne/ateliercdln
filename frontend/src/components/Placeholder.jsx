8ab7f956f5af54d8f324d5f833061e0319725b0d
davidcabanne <davidcbne@gmail.com>
2024-03-03 17:59:04 +0100

    feat: sliding feature for placeholder gallery

--- frontend/src/components/Placeholder.jsx
+++ frontend/src/components/Placeholder.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
-
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import Image from "next/image";
-
 import * as _var from "../styles/variables";
 
 const Container = styled.div`
@@ -25,6 +23,12 @@ const Gallery = styled.div`
     height: 100%;
     opacity: 0;
     z-index: 10;
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none;
 
     &:hover {
       opacity: 1;
@@ -36,6 +40,12 @@ const Gallery = styled.div`
     transform: translateX(-8px);
     transition: 175ms ${_var.cubicBezier};
     transition-property: opacity, transform;
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none;
 
     &.active {
       opacity: 1;
@@ -46,6 +56,19 @@ const Gallery = styled.div`
 
 const Placeholder = ({ image, gallery, alt }) => {
   const [hoverIndex, setHoverIndex] = useState(null);
+  const galleryRef = useRef();
+
+  const handleTouchMove = (event) => {
+    const touchLocation = event.touches[0];
+    const galleryBounds = galleryRef.current.getBoundingClientRect();
+    const touchX = touchLocation.clientX - galleryBounds.left;
+    const eachItemWidth = galleryBounds.width / gallery.length;
+
+    const newIndex = Math.floor(touchX / eachItemWidth);
+    if (newIndex !== hoverIndex && newIndex >= 0 && newIndex < gallery.length) {
+      setHoverIndex(newIndex);
+    }
+  };
 
   return (
     <Container>
@@ -58,30 +81,31 @@ const Placeholder = ({ image, gallery, alt }) => {
         placeholder="blur"
         blurDataURL={image.asset.metadata.blurHash}
       />
-      <Gallery>
-        {gallery?.map((image, index) => {
-          return (
-            <React.Fragment key={image._id}>
-              <div
-                style={{ width: `calc(100% / ${gallery.length})` }}
-                index={index}
-                onMouseEnter={() => setHoverIndex(index)}
-                onMouseLeave={() => setHoverIndex(null)}
-              ></div>
-              <Image
-                index={index}
-                src={image.url}
-                alt={alt}
-                fill
-                sizes="(min-width: 600px) 50vw, 100vw"
-                style={{ objectFit: "cover" }}
-                placeholder="blur"
-                blurDataURL={image.metadata.blurHash}
-                className={index === hoverIndex ? "active" : ""}
-              />
-            </React.Fragment>
-          );
-        })}
+      <Gallery ref={galleryRef}>
+        {gallery?.map((image, index) => (
+          <React.Fragment key={image._id}>
+            <div
+              style={{
+                width: `calc(100% / ${gallery.length})`,
+              }}
+              onMouseEnter={() => setHoverIndex(index)}
+              onMouseLeave={() => setHoverIndex(null)}
+              onTouchStart={() => setHoverIndex(index)}
+              onTouchMove={handleTouchMove}
+              onTouchEnd={() => setHoverIndex(null)}
+            ></div>
+            <Image
+              src={image.url}
+              alt={alt}
+              fill
+              sizes="(min-width: 600px) 50vw, 100vw"
+              style={{ objectFit: "cover" }}
+              placeholder="blur"
+              blurDataURL={image.metadata.blurHash}
+              className={index === hoverIndex ? "active" : ""}
+            />
+          </React.Fragment>
+        ))}
       </Gallery>
     </Container>
   );
