1615e0285a42f5018a909c293009750928acd08a
davidcabanne <davidcbne@gmail.com>
2024-03-03 12:46:39 +0100

    feat: HankenGrotesk typeface

--- frontend/src/pages/rootLayout/rootLayout.js
+++ frontend/src/pages/rootLayout/rootLayout.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
+import { Hanken_Grotesk } from "next/font/google";
+const hankenGrotesk = Hanken_Grotesk({ subsets: ["latin"] });
+
 import Header from "@/components/Header";
 import Email from "@/components/Email";
 import Footer from "@/components/Footer";
@@ -13,7 +16,7 @@ const Page = styled.div`
 
 export default function RootLayout({ children }) {
   return (
-    <main className={inter.className}>
+    <main className={hankenGrotesk.className}>
       <Header />
       <Page>{children}</Page>
       <Email />
