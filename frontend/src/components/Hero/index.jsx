9b1de665b3067f3ed7901c1117834421a0b5f53f
davidcabanne <davidcbne@gmail.com>
2024-05-18 13:12:16 +0200

    feat: hero animation for desktop

--- frontend/src/components/Hero/index.jsx
+++ frontend/src/components/Hero/index.jsx
@@ -3,9 +3,9 @@ import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import * as _var from "@/styles/variables";
 
-import useWindowWidth from "@/hooks/useWindowWidth";
-
 import ShapeIcon from "../Icons/ShapeIcon";
+
+import useWindowWidth from "@/hooks/useWindowWidth";
 
 const Container = styled.section`
   position: relative;
@@ -15,35 +15,18 @@ const Container = styled.section`
   gap: ${_var.spaceML};
   padding: 80px ${_var.spaceM};
   overflow: hidden;
-
-  @media ${_var.device.tablet_max} {
-    padding: 80px 0px;
-
-    & .panelPrimary,
-    .panelThird {
-      margin-left: calc(-94px / 2);
-    }
-    & .panelSecondary {
-      position: relative;
-      padding: ${_var.spaceM};
-
-      & svg,
-      h1 {
-        position: absolute;
-        right: calc(-94px / 2);
-      }
-      & h1 {
-        right: calc((94px / 2) + ${_var.spaceM});
-      }
-    }
-  }
 `;
 
 const Panel = styled.div`
   display: flex;
   justify-content: ${(props) => props.$justifycontent};
-  align-items: center;
-  gap: ${_var.spaceM};
+
+  & div {
+    width: min-content;
+    display: flex;
+    align-items: center;
+    gap: ${_var.spaceM};
+  }
 
   & h1 {
     white-space: nowrap;
@@ -52,75 +35,28 @@ const Panel = styled.div`
 
 const Hero = () => {
   const firstPanelRef = useRef(null);
-  const secondaryPanelRef = useRef(null);
+  const secondPanelRef = useRef(null);
   const thirdPanelRef = useRef(null);
 
   const innerWidth = useWindowWidth();
 
-  const handlePanelWidth = (ref) => {
-    const panelWidth =
-      ref.current?.children[0].getBoundingClientRect().width +
-      32 +
-      ref.current?.children[1].getBoundingClientRect().width;
-
-    return panelWidth;
+  const panelWidth = (ref) => {
+    const panelWidth = ref.current.getBoundingClientRect().width;
+    const contentWidth = ref.current.children[0].getBoundingClientRect().width;
+    return panelWidth - contentWidth;
   };
 
-  const handleTransition = (ref, width, innerWidth) => {
-    const layoutWidth = innerWidth - 128;
+  useEffect(() => {
+    if (firstPanelRef?.current) {
+      const totalWidth = panelWidth(firstPanelRef);
 
-    if (ref === secondaryPanelRef) {
-      if (width < layoutWidth) {
-        const animation = ref.current.animate(
+      if (totalWidth >= 0) {
+        firstPanelRef.current.animate(
           [
             { transform: `translateX(0px)` },
-            { transform: `translateX(${width - layoutWidth}px)` },
-          ],
-          {
-            fill: "forwards",
-            easing: "linear",
-            direction: "alternate",
-            duration: 10000,
-            iterations: Infinity,
-          }
-        );
-      }
-      if (width >= layoutWidth) {
-        const animation = ref.current.animate(
-          [
-            { transform: `translateX(0px)` },
-            { transform: `translateX(${64 - layoutWidth}px)` },
-          ],
-          {
-            fill: "forwards",
-            easing: "linear",
-            direction: "alternate",
-            duration: 10000,
-            iterations: Infinity,
-          }
-        );
-      }
-    } else {
-      if (width < layoutWidth) {
-        const animation = ref.current.animate(
-          [
-            { transform: `translateX(0px)` },
-            { transform: `translateX(${layoutWidth - width}px)` },
-          ],
-          {
-            fill: "forwards",
-            easing: "linear",
-            direction: "alternate",
-            duration: 10000,
-            iterations: Infinity,
-          }
-        );
-      }
-      if (width >= layoutWidth) {
-        const animation = ref.current.animate(
-          [
-            { transform: `translateX(0px)` },
-            { transform: `translateX(${layoutWidth - 64}px)` },
+            {
+              transform: `translateX(${totalWidth}px)`,
+            },
           ],
           {
             fill: "forwards",
@@ -132,45 +68,77 @@ const Hero = () => {
         );
       }
     }
-  };
+    if (secondPanelRef?.current) {
+      const totalWidth = panelWidth(secondPanelRef);
 
-  useEffect(() => {
-    const firstPanelWidth = handlePanelWidth(firstPanelRef);
-    const secondPanelWidth = handlePanelWidth(secondaryPanelRef);
-    const thirdPanelWidth = handlePanelWidth(thirdPanelRef);
+      if (totalWidth >= 0) {
+        secondPanelRef.current.animate(
+          [
+            { transform: `translateX(0px)` },
+            {
+              transform: `translateX(-${totalWidth}px)`,
+            },
+          ],
+          {
+            fill: "forwards",
+            easing: "linear",
+            direction: "alternate",
+            duration: 10000,
+            iterations: Infinity,
+            delay: 300,
+          }
+        );
+      }
+    }
+    if (thirdPanelRef?.current) {
+      const totalWidth = panelWidth(thirdPanelRef);
 
-    handleTransition(firstPanelRef, firstPanelWidth, innerWidth);
-    handleTransition(secondaryPanelRef, secondPanelWidth, innerWidth);
-    handleTransition(thirdPanelRef, thirdPanelWidth, innerWidth);
-  }, [innerWidth]);
+      if (totalWidth >= 0) {
+        thirdPanelRef.current.animate(
+          [
+            { transform: `translateX(0px)` },
+            {
+              transform: `translateX(${totalWidth}px)`,
+            },
+          ],
+          {
+            fill: "forwards",
+            easing: "linear",
+            direction: "alternate",
+            duration: 10000,
+            iterations: Infinity,
+            delay: 600,
+          }
+        );
+      }
+    }
+  }, [firstPanelRef, secondPanelRef, thirdPanelRef, innerWidth]);
 
   return (
     <Container>
-      <Panel
-        ref={firstPanelRef}
-        $justifycontent="start"
-        className="panelPrimary"
-      >
-        <ShapeIcon shape="circle" fill="black" width="94px" minWidth="94px" />
-        <h1>Atelier CDLN</h1>
+      <Panel ref={firstPanelRef} $justifycontent="start">
+        <div>
+          <ShapeIcon shape="circle" fill="black" width="94px" minWidth="94px" />
+          <h1>Atelier CDLN</h1>
+        </div>
       </Panel>
-      <Panel
-        ref={secondaryPanelRef}
-        $justifycontent="end"
-        className="panelSecondary"
-      >
-        <ShapeIcon shape="square" fill="black" width="94px" minWidth="94px" />
-        <h1>Design graphique</h1>
+      <Panel ref={secondPanelRef} $justifycontent="end">
+        <div>
+          <ShapeIcon shape="square" fill="black" width="94px" minWidth="94px" />
+          <h1>Design graphique</h1>
+        </div>
       </Panel>
-      <Panel ref={thirdPanelRef} $justifycontent="start" className="panelThird">
-        <ShapeIcon
-          shape="triangle"
-          fill="black"
-          width="94px"
-          minWidth="94px"
-          rotate
-        />
-        <h1>Direction artistique</h1>
+      <Panel ref={thirdPanelRef} $justifycontent="start">
+        <div>
+          <ShapeIcon
+            shape="triangle"
+            fill="black"
+            width="94px"
+            minWidth="94px"
+            rotate
+          />
+          <h1>Direction artistique</h1>
+        </div>
       </Panel>
     </Container>
   );
